d4b7ef923f82d740cab04c08fce4dc67d5df31bc
Cedric McMillan Jnr <cedric@boxlegal.co.uk>
2017-11-30 11:13:58 +0000

    fixed the include locations for codelibrary

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 #include these files from codelibrary
-require_once($_SERVER["DOCUMENT_ROOT"]	.	"/codelibrary/includes/php/autoload.php");
-require_once($_SERVER["DOCUMENT_ROOT"]	.	"/codelibrary/includes/php/common.php");
+
+require_once($_SERVER["DOCUMENT_ROOT"]	.	"/../codelibrary/includes/php/autoload.php");
+require_once($_SERVER["DOCUMENT_ROOT"]	.	"/../codelibrary/includes/php/common.php");
 require_once("/config.inc.php");
 
 include ("app/includes/php/template.php");
